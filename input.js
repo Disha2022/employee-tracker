8cfded944f47602cc412ef9f2f37e70cd1ed6190
disha <mumudb@gmail.com>
2022-05-15 15:00:25 -0400

    add department and role WIP

--- input.js
+++ input.js
@@ -13,28 +13,71 @@ inquirer
   .prompt([
     {
         type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['View all departments', 'View all roles', 'View all employees','add a department', 'add a role', 'add an employee', 'update an employee role']
-    },
-    {
+        name: 'main',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees',
+        'Add a department', 'Add a role', 'Add an employee', 
+        'Update an employee role']
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    if (answers.main === 'Add a department'){
+      inquirer
+      .prompt([{
         type: 'input',
         name: 'department',
         message: 'What is name of department?',
       },
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    
-    console.log(answers.size)
-
-    connection.query(
-        `INSERT INTO departments(d_name) VALUES ${answers.name}`,
+    ]).then((answers) => {
+      connection.query(
+        `INSERT INTO department(name) VALUES ${answers.name}`,
         function(err, results, fields) {
+          console.log("Added " + results + " to the database.")
           console.log(results); // results contains rows returned by server
           console.log(fields); // fields contains extra meta data about results, if available
         }
       );
+    })  
+    }
+
+    if (answers.main === 'Add a role'){
+      inquirer
+      .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is name of the role?',
+      },{
+        type: 'input',
+        name: 'salary',
+        message: 'What is salary of the role?',
+      },{
+        type: 'input',
+        name: 'department',
+        message: 'What department does the role belong to?',
+      },
+    ]).then((answers) => {
+      let id;
+      connection.query(
+        'SELECT id FROM `department` WHERE `name` = ?',
+        [answers.department],
+        function(err, results) {
+          console.log(results);
+        }
+      );
+
+      connection.query(
+        `INSERT INTO role(title, salary, department_id) VALUES ${answers.name}`,
+        'INSERT INTO department(name) VALUES ?, ? , ?',
+        [answers.name, answers.salary, id],
+        function(err, results) {
+          console.log("Added " + answers.name + " to the database.")
+          console.log(results);
+        }
+      );
+    })  
+    }
+
   })
   .catch((error) => {
     if (error.isTtyError) {
